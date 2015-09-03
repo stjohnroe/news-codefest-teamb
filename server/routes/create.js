a1d4b8556fe4ed60b8ce8157503e5109f83c7543
stjohnroe <stjohn.roe@news.co.uk>
2015-09-03 16:54:44 +0100

    added presigned url generation

--- server/routes/create.js
+++ server/routes/create.js
@@ -1,12 +1,21 @@
-var express = require('express');
-var request = require('request');
-var async = require('async');
-var router = express.Router();
+module.exports = function(req, res, next) {
+	var request = require('request');
+	var async = require('async');
+	var aws = require('aws-sdk');
+	aws.config.region = 'eu-west-1'
 
-router.get('/', function(req, res, next) {
-  res.render('create',
-    { title: 'The Sun'
-    });
-});
-
-module.exports = router;
+	var storyId = req.params.id
+	for (i in req.params) {
+		console.log(i)
+	}
+	var userId = "dave"
+	var s3 = new aws.S3();
+	var params = {Bucket: 'codefestb', Key: 'articles/'+storyId+'/'+userId };
+	var submitUrl
+	s3.getSignedUrl('putObject', params, function (err, url) {
+	  console.log("The URL is", url);
+	  submitUrl = url
+	});
+	
+	res.render('create', { title: 'The Sun', submitPath: submitUrl});
+};
